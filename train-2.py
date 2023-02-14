fae90cd9138589d4a2ac20cccbc7a3997016d818
Francisco Arnaldo Boix Martinez <quicomurciai@gmail.com>
2023-02-14 23:06:02 +0100

    first attempt to refactor

---
+++ train-2.py
@@ -0,0 +1,18 @@
+
+import argparse
+import os
+
+from AMPE_dnn import My_DNN
+
+
+def main():
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-e", "--epochs", help="number of epochs", type=int, required=True)
+    args = parser.parse_args()
+
+    model = My_DNN(epochs=args.epochs, file=f"pesos-{args.epochs}.pt")
+    model.train()
+
+if __name__ == '__main__':
+    main()
